80d25e730e25dd784b464cb003b8696f0c497e3f
Samuel Brice <sbrice@ny-hanjum.ondeck.local>
2015-11-03 16:41:48 -0500

    yo init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('zestAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
